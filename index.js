669e71663b9b4a6fe5ce1924b27e67604c2a2294
Daryll Wong <daryll_wong@hotmail.com>
2020-05-14 21:55:11 +0800

    Template 1(yellow)

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 $(document).ready(function() {
-    var scroll_start = 0;
-    var startchange = $('#startchange');
-    var offset = startchange.offset();
-    if (startchange.length) {
-        $(document).scroll(function() {
-            scroll_start = $(this).scrollTop();
-            if (scroll_start > offset.top) {
-                $(".navbar-default").css('background-color', '#c1292e');
-            } else {
-                $('.navbar-default').css('background-color', 'transparent');
-            }
+    // Add scroll to every link
+    $("a").click(function() {
+        // Make sure hash value exists
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior if JS enabled, otherwise fallback to HTML
+            event.preventDefault();
+            // Store hash for location
+            var hash = this.hash;
+            //Animate scroll
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - $('.nav').height()
+            }, 800, function() {});
+        } // End if
+    });
+
+    //Function set such that when menu bars are clicked, mobileNav menu is toggled sliding left and right
+    $('.fa-bars').click(function() {
+        $('.mobileNav').animate({
+            width: "toggle"
         });
-    }
+    });
+
+    // Sets the offset position of the navigation bar
+    var offset = $('.nav').offset();
+    var sticky = offset.top;
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    $(window).bind('scroll', function() {
+        if ($(window).scrollTop() > sticky) {
+            $('.nav').addClass('sticky');
+        } else {
+            $('.nav').removeClass('sticky');
+        }
+    });
 });
